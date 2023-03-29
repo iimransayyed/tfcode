5c67e30af106f74469bca0fbdb58f764f9942b5d
iimransayyed <iimransayyed@outlook.com>
2023-03-29 22:43:30 +0530

    created pvt and pub subnets for vpc

--- vpc/vpc-apsae1/main.tf
+++ vpc/vpc-apsae1/main.tf
@@ -5,6 +5,13 @@ module "vpc_staging" {
 
   environment_name         = local.vpc_name
   vpc_cidr                 = "10.1.0.0/16"
+  public_subnet_cidr_az_a  = ["10.1.0.0/22"]
+  public_subnet_cidr_az_b  = ["10.1.4.0/22"]
+  public_subnet_cidr_az_c  = ["10.1.8.0/22"]
+  private_subnet_cidr_az_a = ["10.1.12.0/22"]
+  private_subnet_cidr_az_b = ["10.1.16.0/22"]
+  private_subnet_cidr_az_c = ["10.1.20.0/22"]
+
   additional_tags = {
     Environment = "Staging"
     Owner       = "RnDAdmin"
